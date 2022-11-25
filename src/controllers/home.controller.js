7a61bfdeeb72bb796f47ec086efe72665cecc2f4
tranthudat2k1 <thudatdl123@gmail.com>
2022-11-25 22:58:20 +0700

    add page hoa thuc vat

--- src/controllers/home.controller.js
+++ src/controllers/home.controller.js
@@ -1,3 +1,7 @@
 module.exports.get = (req, res, next) => {
     res.render("home/index.ejs")
   }
+
+  module.exports.getHoaSinhHoc = (req, res, next) => {
+    res.render("hoa-sinh-hoc/index.ejs")
+  }
--- src/routes/home.router.js
+++ src/routes/home.router.js
@@ -2,3 +2,4 @@ const home = require('../controllers/home.controller')
 
 module.exports= require('express').Router()
   .get("/", home.get)
+  .get("/hoa-thuc-vat-va-hoat-tinh-sinh-hoc", home.getHoaSinhHoc)
